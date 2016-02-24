07dd88b9c0ec0894fd21cff0967a061bac0ed2e5
Jongsoo Park <jongsoo.park@intel.com>
2016-02-23 22:44:08 -0800

    split coo files into groups; add script to run experiments

---
+++ experiments/sparsity/matrixmarket/alexnet/0.54214_1.0e-3/csrmm_test.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+./csrmm_test coo_conv1.weight 3025
+echo
+./csrmm_test coo_conv2_1.weight 729
+echo
+./csrmm_test coo_conv2_2.weight 729
+echo
+./csrmm_test coo_conv3.weight 169
+echo
+./csrmm_test coo_conv4_1.weight 169
+echo
+./csrmm_test coo_conv4_2.weight 169
+echo
+./csrmm_test coo_conv5_1.weight 169
+echo
+./csrmm_test coo_conv5_2.weight 169
