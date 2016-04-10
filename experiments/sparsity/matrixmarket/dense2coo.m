e46bb71dc2cd29c18f6ec0aa8707195521149bcc
wenwei202 <weiwen.web@gmail.com>
2016-04-10 12:41:05 -0400

    add alexnet without accuracy drop and medium sparsity

--- experiments/sparsity/matrixmarket/dense2coo.m
+++ experiments/sparsity/matrixmarket/dense2coo.m
@@ -1,7 +1,14 @@
-function  dense2coo(filename)
+function  dense2coo(filename,ydimen,xdimen)
     [A,rows,cols,entries,rep,field,symm] = mmread(filename);
-    %comment='%MatrixMarket matrix coordinate real general';
+    mmwrite(sprintf('coo_%s',filename),sparse(A));
+    
+    [A,rows,cols,entries,rep,field,symm] = mmread(sprintf('coo_%s',filename));
+    A = full(A);
     colormap('gray')
     imshow(A==0);
-   mmwrite(sprintf('coo_%s',filename),sparse(A));
+    display('K-mode sparsity:')
+    display(block_sparsity(A, ydimen,xdimen));
+    display('Elewise sparsity:')
+    display(sum(sum(abs(A==0)))/numel(A));
+    display('-----------------')
 end
---
+++ experiments/sparsity/matrixmarket/dense2coo_alexnet_conv.m
@@ -0,0 +1,7 @@
+folder = '0.57278[0.0003]_0.562519[0.0001]';
+dense2coo('conv1.weight',1,121);
+dense2coo('conv2.weight',1,25);
+dense2coo('conv3.weight',1,9);
+dense2coo('conv4.weight',1,9);
+dense2coo('conv5.weight',1,9);
+close all;
