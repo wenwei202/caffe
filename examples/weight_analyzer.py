5dec72d310f98e5c0c807424fd9fbfffde7679ac
wenwei202 <weiwen.web@gmail.com>
2016-04-25 20:30:08 -0400

    weight_analyzer.py

--- examples/weight_analyzer.py
+++ examples/weight_analyzer.py
@@ -1,116 +1,124 @@
-__author__ = 'wew57'
-'''
-analyze the weights of caffemodel
-'''
-from numpy import *
-import matplotlib.pyplot as plt
-from scipy.io import *
-
-from PIL import Image
-#import sys
+__author__ = 'pittnuts'
 import caffe
-from pittnuts import *
 import re
-from decimal import *
-# configure plotting
-plt.rcParams['figure.figsize'] = (10, 10)
-plt.rcParams['image.interpolation'] = 'nearest'
-plt.rcParams['image.cmap'] = 'gray'
+from pittnuts import *
+import os
+import matplotlib.pyplot as plt
+import argparse
+import caffeparser
+# --prototxt models/bvlc_reference_caffenet/deploy.prototxt --origimodel models/bvlc_reference_caffenet/bvlc_reference_caffenet.caffemodel --tunedmodel /home/wew57/github/caffe/models/bvlc_reference_caffenet/caffenet_train_grouplasso_iter_120000.caffemodel
+# --prototxt examples/mnist/lenet.prototxt --origimodel examples/mnist/lenet_0.9912.caffemodel --tunedmodel examples/mnist/
+# --prototxt examples/cifar10/cifar10_full.prototxt --origimodel examples/cifar10/cifar10_full_iter_300000_0.8212.caffemodel --tunedmodel examples/cifar10/cifar10_full_grouplasso_iter_60000.caffemodel
+def show_filters(net,layername ,filt_min ,filt_max,rgb=False):
+    weights = net.params[layername][0].data
+    if len(weights.shape) < 3:
+        return
+    chan_num = weights.shape[1]
+    filter_num = weights.shape[0]
+    #display_region_size = ceil(sqrt(filter_num))
+    plt.figure()
+    for c in range(min(20,chan_num)):
+        #if sum(abs(weights[:,c,:,:]))>0:
+            #print "{}-th channel is usefull".format(c)
+            for n in range(filter_num):
+                #plt.subplot((int)(display_region_size),(int)(display_region_size),n+1)
+                plt.subplot(chan_num,filter_num, filter_num*c + n + 1)
+                #if sum(abs(weights[n,c]))>0:
+                plt.imshow(weights[n,c],vmin=filt_min,vmax=filt_max,cmap=plt.get_cmap('Greys'),interpolation='none')
+                plt.tick_params(which='both',labelbottom='off',labelleft='off',bottom='off',top='off',left='off',right='off')
 
-# Load the net, list its data and params, and filter an example image.
-# caffe.set_mode_cpu()
-# GPU mode
-caffe.set_device(0)
-caffe.set_mode_gpu()
+def show_filter_pca(net,layername):
+    weights = net.params[layername][0].data
+    if len(weights.shape) < 3:
+        return
+    filter_num = weights.shape[0]
+    chan_num = weights.shape[1]
+    kernel_h = weights.shape[2]
+    kernel_w = weights.shape[3]
+    kernel_size = kernel_h*kernel_w
 
-#net = caffe.Net('../models/bvlc_reference_caffenet/deploy.prototxt','../models/bvlc_reference_caffenet/bvlc_reference_caffenet.caffemodel', caffe.TEST)
-#net = caffe.Net('models/eilab_reference_sparsenet/deploy_scnn.prototxt','models/eilab_reference_sparsenet/sparsenet_train_iter_1000.caffemodel', caffe.TEST)
-net = caffe.Net('/home/wew57/bincaffe_bin_copyed/models/bvlc_reference_caffenet/train_val.prototxt','/home/wew57/bincaffe_bin_copyed/models/bvlc_reference_caffenet/caffenet_train_iter_120000.caffemodel', caffe.TEST)
-#net = caffe.Net('../examples/cifar10/cifar10_full_scnn.prototxt','../examples/cifar10/eilab_cifar10_full_ini_sparsenet.caffemodel', caffe.TEST)
-print("blobs {}\nparams {}".format(net.blobs.keys(), net.params.keys()))
-coefficient = 0.00005
-group_weight_decay = 0.00001
-sum_l1 = 0
-sum_l2 = 0
-savetxt("conv3.weight",net.params['conv3'][0].data.reshape((384,256*9)),fmt='%.10f')
-for idx in range(0,len(net.params.keys())):
-    key = net.params.keys()[idx]
-    if re.match("^conv[1-9]$",key) or re.match("^fc[1-9]$",key):
-        print '{}:'.format(key)
-        weights = net.params[key][0].data
-        sum_l2 += sum(weights**2)*0.5*coefficient
-        sum_l1 +=  sum(abs(weights))*coefficient
-        #print "mean: {}".format(abs(weights).mean())
-        #print "sum of abs {}".format(sum(abs(weights)))
-        group_lasso_term = 0
-        print "l2:{} l1:{}".format(sum(weights**2)*0.5*coefficient,sum(abs(weights))*coefficient)
-        if re.match("^conv[1-9]$",key):
-            temp = zeros((1,weights.shape[1]))
-            temp_diff = zeros((weights.shape[0],weights.shape[1]))
-            for c in range(0,weights.shape[1]):
-                    tmp=weights[:,c,:,:].flatten()
-                    tmp=dot(tmp,tmp)
-                    temp[0,c]= sqrt(tmp)
-                    group_lasso_term += temp[0,c]
-
-            for n in range(0,weights.shape[0]):
-                for c in range(0,weights.shape[1]):
-                    if temp[0,c]==0:
-                        temp_diff[n,c]=0
-                    else:
-                        temp_diff[n,c]=weights[n,c,0,0]/temp[0,c]
-
-
-        print "group_lasso_term = {}".format(group_lasso_term*group_weight_decay)
-
-        #print "sum of temp: {}".format(sum(abs(temp.flatten()))*96)
-        #print "sum of temp: {}".format(sum(abs(temp_diff.flatten())))
-        print "asum of diff : {}".format(sum(abs(group_weight_decay*temp_diff.flatten())))
-
-print "\n\nl2 regularization: {}\nl1 regularization: {}".format(sum_l2,sum_l1)
+    weights_pca = weights.reshape((chan_num*filter_num, kernel_size)).transpose()
+    weights_pca, eig_vecs, eig_values = pca(weights_pca)
+    print eig_values
+    weights_pca = weights_pca.transpose().reshape(filter_num,chan_num,kernel_h,kernel_w)
+    filt_max = abs(weights_pca).max()
+    filt_min = -filt_max
+    #eig_vecs = eig_vecs.transpose().reshape(kernel_size,kernel_h,kernel_w)
+    plt.figure()
+    for c in range(min(20, chan_num)):
+        for n in range(filter_num):
+            plt.subplot(chan_num, filter_num, filter_num * c + n + 1)
+            plt.imshow(weights_pca[n, c], vmin=filt_min, vmax=filt_max, cmap=plt.get_cmap('Greys'), interpolation='none')
+            plt.tick_params(which='both', labelbottom='off', labelleft='off', bottom='off', top='off', left='off',right='off')
 
 
+def show_filter_shapes(net, layername):
+    weights = net.params[layername][0].data
+    if len(weights.shape) < 3:
+        return
+    chan_num = weights.shape[1]
+    filter_num = weights.shape[0]
+    weights = abs(weights)
+    weights = sum(weights,axis=0)!=0
+    plt.figure()
+    for c in range(min(20, chan_num)):
+        plt.subplot(chan_num, 1, c + 1)
+        plt.imshow(weights[c], vmin=0, vmax=1, cmap=plt.get_cmap('Greys'), interpolation='none')
+        plt.tick_params(which='both', labelbottom='off', labelleft='off', bottom='off', top='off', left='off', right='off')
+
+# get the maximum abs weight
+def get_max_weight(orig_net,tuned_net,layer_name):
+    weight_scope = 0
+    weights_orig = orig_net.params[layer_name][0].data
+    weights_tuned = tuned_net.params[layer_name][0].data
+    max_val = abs(weights_orig).max()
+    if max_val > weight_scope:
+        weight_scope = max_val
+    max_val = abs(weights_tuned).max()
+    if max_val > weight_scope:
+        weight_scope = max_val
+    return weight_scope
 
 
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--prototxt', type=str, required=True)
+    parser.add_argument('--origimodel', type=str, required=True)
+    parser.add_argument('--tunedmodel', type=str, required=True)
+    args = parser.parse_args()
+    prototxt = args.prototxt #"models/eilab_reference_sparsenet/train_val_scnn.prototxt"
+    original_caffemodel = args.origimodel # "models/eilab_reference_sparsenet/eilab_reference_sparsenet.caffemodel"
+    fine_tuned_caffemodel = args.tunedmodel # "/home/wew57/2bincaffe/models/eilab_reference_sparsenet/sparsenet_train_iter_30000.caffemodel"
+    net_parser = caffeparser.CaffeProtoParser(prototxt)
+    net_msg = net_parser.readProtoNetFile()
+
+    caffe.set_mode_cpu()
+    # GPU mode
+    #caffe.set_device(1)
+    #caffe.set_mode_gpu()
+
+    orig_net = caffe.Net(prototxt,original_caffemodel, caffe.TEST)
+    tuned_net = caffe.Net(prototxt,fine_tuned_caffemodel, caffe.TEST)
+
+    print("blobs {}\nparams {}".format(orig_net.blobs.keys(), orig_net.params.keys()))
+    print("blobs {}\nparams {}".format(tuned_net.blobs.keys(), tuned_net.params.keys()))
+
+    #show_filter_pca(orig_net, 'conv1')
+    #show_filter_pca(tuned_net, 'conv1')
+    #show_filter_pca(orig_net, 'conv2')
+    #show_filter_pca(tuned_net, 'conv2')
+
+    weight_scope = get_max_weight(orig_net, tuned_net, 'conv1')
+    #show_filters(orig_net,'conv1',-weight_scope, weight_scope)
+    #show_filters(tuned_net, 'conv1', -weight_scope, weight_scope)
+    show_filter_shapes(orig_net, 'conv1')
+    show_filter_shapes(tuned_net, 'conv1')
 
 
-#for idx in range(0,5):
-#    data = net.params[net.params.keys()[idx]][0].data
-#    #if idx != 0:
-#    #    data = data[0:data.shape[0]/2,:,:,:]
-#    weights = transpose(data,(2,3,1,0))
-#    P,S,Q,q = kernel_factorization(weights)
-#    print weights.shape
-#    #print q
-#    weights_recover,R = kernel_recover(P,S,Q,q)
-#    print "[{}] W: %{} error".format(net.params.keys()[idx],100*sum((weights-weights_recover)**2)/sum((weights)**2))
-#    r_width = 0.0001
-#    print "[{}] S: %{} zeros".format(net.params.keys()[idx],100*sum((abs(S)<r_width).flatten())/(float)(S.size))
-#    print "[{}] R: %{} zeros".format(net.params.keys()[idx],100*sum((abs(R)<r_width).flatten())/(float)(R.size))
-#
-#for key_idx in range(0,len(net.blobs.keys())):
-#    print net.blobs.keys()[key_idx], ' -> ', net.blobs[net.blobs.keys()[key_idx]].data.shape
-#    #print net.blobs[net.blobs.keys()[key_idx]].data.shape
+    #weight_scope = get_max_weight(orig_net, tuned_net, 'conv2')
+    #show_filters(orig_net, 'conv2', -weight_scope, weight_scope)
+    #show_filters(tuned_net, 'conv2', -weight_scope, weight_scope)
+    #show_filter_shapes(orig_net, 'conv2')
+    #show_filter_shapes(tuned_net, 'conv2')
 
-
-## display bias and weights of conv1
-## print net.params['conv1'][1].data
-#weights = transpose(net.params['conv1'][0].data)
-##savemat('./conv1weights.mat',{'data':weights})
-
-#
-##plt.hist(log10(abs(R)).flatten(),bins=100)
-##plt.hist(log10(abs(weights)).flatten(),bins=100)
-#plt.hist(S.flatten(),bins=1000,range=(-r_width,r_width))
-#print sum((abs(S)<r_width).flatten())
-#print S.size
-##print abs(S).max(),abs(S).min()
-##plt.hist(weights.flatten(),bins=1000)
-##print sum(weights)
-##print sum(weights_recover)
-##h = histogram(S,bins=1000,range=(-r_width,r_width))
-##print h
-
-
-
-#plt.show()
+    plt.show()
