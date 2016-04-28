9367a0b338c952f0f891224b0dedb501020bf1d3
Jongsoo Park <jongsoo.park@intel.com>
2016-04-28 14:10:55 -0700

    an attempt to not use boost thread; not completely done

--- src/caffe/common.cpp
+++ src/caffe/common.cpp
@@ -10,13 +10,18 @@
 namespace caffe {
 
 // Make sure each thread can have different values.
-static boost::thread_specific_ptr<Caffe> thread_instance_;
+//static boost::thread_specific_ptr<Caffe> thread_instance_;
+Caffe *thread_instance_ = NULL;
 
 Caffe& Caffe::Get() {
-  if (!thread_instance_.get()) {
-    thread_instance_.reset(new Caffe());
+  if (!thread_instance_) {
+    thread_instance_ = new Caffe();
   }
-  return *(thread_instance_.get());
+  return *thread_instance_;
+//  if (!thread_instance_.get()) {
+//    thread_instance_.reset(new Caffe());
+//  }
+//  return *(thread_instance_.get());
 }
 
 // random seeding
