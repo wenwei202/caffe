b69e0945571adb5e5d475934d05ed790fbd36863
Wei Wen <weiwen.web@gmail.com>
2016-11-02 10:07:33 -0400

    hdf5 bug of googlenet

--- models/bvlc_googlenet/train_script_force.sh
+++ models/bvlc_googlenet/train_script_force.sh
@@ -55,4 +55,4 @@ fi
 cat ${snapshot_path}/train.info | grep loss+ | awk '{print $8 " " $11}' > ${snapshot_path}/loss.info
 python python/plot_train_info.py --traininfo ${snapshot_path}/train.info
 content="$(hostname) done: ${0##*/} ${@}. Results in ${snapshot_path}"
-echo ${content} | mail -s "Training done" weiwen.web@gmail.com
+echo ${content} | mail -s "Training done" adddress@example.com
